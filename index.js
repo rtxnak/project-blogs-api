1e2f93133afd810076f84dbaeef2bdf8a10fbcf3
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-04-19 19:31:39 -0300

    feat: adicionou no index o ENDPOINT GET /user

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
@@ -8,3 +9,7 @@ app.listen(3000, () => console.log('ouvindo porta 3000!'));
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use(express.json());
+
+app.use('/user', userRoutes);
