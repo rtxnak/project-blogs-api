1c7381732f074ca086fc24fee648b41900ea44f7
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-04-20 17:40:14 -0300

    feat: adicionou controller do ENPOINT POST /user

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,6 +9,20 @@ const getAll = async (_req, res) => {
   }
 };
 
+const create = async (req, res) => {
+  try {
+    const { displayName, email, password, image } = req.body;
+
+    const created = await userService.create({ displayName, email, password, image });
+    
+    return res.status(201).json(created);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: 'Erro no Servidor' });
+  }
+};
+
 module.exports = {
   getAll,
+  create,
 };
