f40f1e284a850b52ef30fc8c37c7b934f045a6d0
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-04-20 17:41:57 -0300

    feat: adicionou route do ENPOINT POST /user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const userController = require('../controllers/userController');
 
+const {
+  isValidDisplayName,
+  isValidPassword,
+  isValidEmail,
+} = require('../middlewares/userValidation');
+
 const routes = express.Router();
 
-routes.get('/', userController.getAll);
+routes
+  .get('/', userController.getAll)
+  .post('/',
+    isValidDisplayName,
+    isValidPassword,
+    isValidEmail,
+    userController.create);
 
 module.exports = routes;
