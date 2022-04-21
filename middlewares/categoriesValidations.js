f0ec0d0a246387714d23e6d5bc98b69fc269c617
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-04-21 18:20:52 -0300

    feat: adicionou middleware de validacao de campo  para o ENDPOINT POST no /categories

---
+++ middlewares/categoriesValidations.js
@@ -0,0 +1,15 @@
+const isValidName = (req, res, next) => {
+  const { name } = req.body;
+
+  if (name === undefined) {
+    return res.status(400).json(
+      { message: '"name" is required' },
+    );
+  }
+
+  next();
+};
+
+module.exports = {
+  isValidName,
+};
